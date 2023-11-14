73a5d581b75ba90af5b6fd8b8c217bba1c7cb366
Bora Canbula <bora.canbula@cbu.edu.tr>
2023-11-14 18:26:11 +0300

    Version 0.4

--- ieee754/__init__.py
+++ ieee754/__init__.py
@@ -1,1 +1,6 @@
 from ieee754.IEEE754 import IEEE754
+from ieee754.IEEE754 import half
+from ieee754.IEEE754 import single
+from ieee754.IEEE754 import double
+from ieee754.IEEE754 import quadruple
+from ieee754.IEEE754 import octuple
--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name="ieee754",
     packages=["ieee754"],
-    version="0.3",
+    version="0.4",
     license="MIT",
     description="A Python module which converts floating points numbers into IEEE-754 representation.",
     author="Bora Canbula",
     author_email="bora.canbula@cbu.edu.tr",
     url="https://github.com/canbula/ieee754",
-    download_url="https://github.com/canbula/ieee754/archive/refs/tags/v_03.tar.gz",
+    download_url="https://github.com/canbula/ieee754/archive/refs/tags/v_04.tar.gz",
     keywords=[
         "IEEE-754",
         "precisions",
